c648d413320b2e8f683fb31860a9779b12cb48d8
Sanjay Kumar <skh1827532@gmail.com>
2024-06-11 19:17:56 +0500

    Pushed requirements.txt

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ def detect_edges_and_contours(image_cv, resize_width=360, resize_height=480):
 
     return image_with_contours, resized_img, resized_edges, closed, filtered_contours
 
-@app.get("/show-image")
+@app.post("/show-image")
 async def show_image(data: ImageData):
     try:
         # Decode the base64 string
